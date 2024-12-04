820216f93455075e6fc1ef0dad651ba451b09cc3
Evgeny Starikov <genestar@yandex.ru>
2024-12-04 18:46:02 +0300

    Initial commit

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,4 +1,5 @@
 const sorting = require("../../app");
+const { TestWatcher } = require("jest");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
@@ -6,12 +7,23 @@ describe("Books names test suit", () => {
       sorting.sortByName([
         "Гарри Поттер",
         "Властелин Колец",
-        "Волшебник изумрудного города",
+        "Волшебник изумрудного города"
       ])
     ).toEqual([
       "Властелин Колец",
       "Волшебник изумрудного города",
-      "Гарри Поттер",
+      "Гарри Поттер"
     ]);
+  });
+});
+describe("Books names test suit negative", () => {
+  it("Naming a workbook without sorting", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Волшебник изумрудного города",
+        "Гарри Поттер"
+      ])
+    ).toEqual(["Волшебник изумрудного города", "Гарри Поттер", "Гарри Поттер"]);
   });
 });
