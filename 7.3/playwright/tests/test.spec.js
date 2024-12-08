2439ce7b16ee0a49e96aaefc92a6a9bf8397113e
Evgeny Starikov <genestar@yandex.ru>
2024-12-08 13:35:20 +0300

    Playwrigts tests modified

--- 7.3/playwright/tests/test.spec.js
+++ 7.3/playwright/tests/test.spec.js
@@ -1,27 +1,30 @@
-import { test, expect } from "@playwright/test";
-//const { chromium } = require("playwright");
+const { test, expect } = require("@playwright/test");
+const {
+  invalidName,
+  invalidPassword,
+  validName,
+  validPassword
+} = require("../auth");
 
-const { email, password } = require("../auth");
-
-test("successful authorization", async ({ page }) => {
-  //const page = await browser.newPage("https://netology.ru/?modal=sign_in");
+test("Failed authorization", async () => {
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.getByPlaceholder("Email").click();
-  await page.getByPlaceholder("Email").fill(email);
-  await page.getByPlaceholder("Пароль").click();
-  await page.getByPlaceholder("Пароль").fill(password);
-  await page.getByTestId("login-submit-btn").click();
-  await expect(page.locator("h2")).toContainText(["Моё обучение"]);
-  browser.close();
-});
-
-/*test("Failed authorization", async () => {
-  //const page = await browser.newPage("https://netology.ru/?modal=sign_in");
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await page.fill('[placeholder="Email"]', incorrectEmail);
-  await page.fill('[placeholder="Пароль"]', incorrectPassport);
+  await page.fill('[placeholder="Email"]', invalidName);
+  await page.fill('[placeholder="Пароль"]', invalidPassword);
   await page.click('[data-testid="login-submit-btn"]');
   const error = await page.locator('[data-testid="login-error-hint"]');
   await expect(error).toHaveText("Вы ввели неправильно логин или пароль");
-  browser.close();
-}); */
+});
+
+test("validUser", async ({ page }) => {
+  await page.goto("https://netology.ru//?modal=sign_in");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(validName);
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill(validPassword);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(
+    page.getByRole("heading", { name: "Моё обучение" })
+  ).toBeVisible();
+});
